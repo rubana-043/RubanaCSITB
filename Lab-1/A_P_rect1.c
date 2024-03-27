5b9233101221afdd4cca0331d730b94587ea81b0
rubana-043 <152357691+rubana-043@users.noreply.github.com>
2024-03-27 18:43:49 +0545

    add comments

--- Lab-1/A_P_rect1.c
+++ Lab-1/A_P_rect1.c
@@ -4,8 +4,11 @@ void main()
 	float l,b,A,P;
 	printf("Enter length and breadth of rectangle: ");
 	scanf("%f%f", &l,&b);
+	//Area of the Rectangle
 	A=l*b;
+	//Perimeter of the Rectangle
 	P=2*(l+b);
+	//Print Area and Perimeter
 	printf("Area of rectangle is: %.2f", A);
 	printf("\nPerimeter of rectangle is: %.2f", P);
 }
