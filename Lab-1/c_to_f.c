89c6ec884a3b29465c4d5890392d9f5c90ee0eb5
rubana-043 <152357691+rubana-043@users.noreply.github.com>
2025-06-23 08:54:24 +0545

    Delete Lab-1 directory

--- Lab-1/c_to_f.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-	float centigrade,fahernheit;
-	printf("Enter temperature in Centigrade: ");
-	scanf("%f", &centigrade );
-	fahernheit=((centigrade*9)/5)+32;
-	printf("%.2f Centigrade= %.2f Fahernheit",centigrade fahernheit);
-}
--- Lab-1/f_to_c.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-	float centigrade,fahernheit;
-	printf("Enter temperature in Fahernheit: ");
-	scanf("%f", &fahernheit);
-	centigrade=(5*(fahernheit-32))/9;
-	printf("%.2f Fahrenheit = %.2f Centigrade", fahernheit, centigrade);
-}
